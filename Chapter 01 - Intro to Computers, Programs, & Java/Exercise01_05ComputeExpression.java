460cfd93744477d5d76cd11edf81e69cd902c293
ZachGundy <82851396+ZachGundy@users.noreply.github.com>
2022-10-03 14:57:53 -0600

    Name Change

--- Chapter 01 - Intro to Computers, Programs, & Java/Exercise01_05ComputeExpression.java
+++ Chapter 01 - Intro to Computers, Programs, & Java/Exercise01_05ComputeExpression.java
@@ -1,6 +1,6 @@
 package JavaFX;
 
-public class Exercise1_05 {
+public class Exercise01_05COmputeExpression  {
     public static void main(String[] args) {
         System.out.println (((9.5 *4.5) - (2.5 * 3)) / (45.5 - 3.5));
     }
