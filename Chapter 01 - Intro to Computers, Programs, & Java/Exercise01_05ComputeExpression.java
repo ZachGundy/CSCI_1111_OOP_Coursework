82ed29c05970ca22d1108d5672798b186a62ae78
ZachGundy <82851396+ZachGundy@users.noreply.github.com>
2022-10-03 15:00:42 -0600

    Delete Exercise01_05ComputeExpression.java

--- Chapter 01 - Intro to Computers, Programs, & Java/Exercise01_05ComputeExpression.java
+++
@@ -1,7 +0,0 @@
-package JavaFX;
-
-public class Exercise01_05COmputeExpression  {
-    public static void main(String[] args) {
-        System.out.println (((9.5 *4.5) - (2.5 * 3)) / (45.5 - 3.5));
-    }
-}
