e19c1e91f0756db901ffd2ebb637bd47571ae904
ZachGundy <82851396+ZachGundy@users.noreply.github.com>
2022-10-03 15:08:45 -0600

    Change Class Name

---
+++ Chapter 01 - Intro to Computers, Programs, & Java/E01_09Area&Perimeter.java
@@ -0,0 +1,9 @@
+package JavaFX;
+
+public class E01_09Area&Permimeter {
+
+public static void main(String[] args) {
+        System.out.println (4.5 * 7.9);
+        System.out.println (2*( 4.5 + 7.9));
+        }
+        }
