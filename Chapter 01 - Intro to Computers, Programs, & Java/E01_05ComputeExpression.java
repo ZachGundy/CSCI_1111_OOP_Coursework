3de452f479e358d4255fbe65b262e8d6aed8c783
ZachGundy <82851396+ZachGundy@users.noreply.github.com>
2022-10-03 15:25:29 -0600

    Add assignment instruction

--- Chapter 01 - Intro to Computers, Programs, & Java/E01_05ComputeExpression.java
+++ Chapter 01 - Intro to Computers, Programs, & Java/E01_05ComputeExpression.java
@@ -1,3 +1,8 @@
+/** (Compute expressions) Write a program that displays the result of 9.5*4.5-2.5*3/45.5-3.5
+ * Result: 0.8392857142857143
+ * Author: Zach Gunderson
+**/
+
 package JavaFX;
 
 public class E01_05ComputeExpression  {
